1541f89cf7a970054865adbcc701cb2921c210ad
Justine Dewilde <justine.dewilde.pro@gmail.com>
2015-06-16 10:05:42 +0200

    création de ant création fonctions relatives aux hormones

---
+++ process/ant.py
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+import random
+from data.distance import randomPondereNoeud
+from data.place import places
+
+__author__ = 'Justine Dewilde'
+
+class Ant:
+    def __init__(self):
+        firstPosition = int(random.random() * len(places))
+        self.visitedPlaces = []
+        self.visitedPlaces.append(firstPosition)
+        self.initializeStillToVisit(firstPosition)
+        self.durationTravel = 0             #tempsTotal
+        self.longTravel = 0                 #distanceTotal
+        self.currPosition = firstPosition   #point courant
+        self.initialPostion = firstPosition #premiere position
+
+    #Fonction permettant d'initialiser la liste des lieux a visiter
+    def initializeStillToVisit(self, firstPosition):
+        nbrPlaces = len(places)
+        i = 0
+        self.placesStillToVisit = []
+
+        while(i < nbrPlaces):
+            if(i != firstPosition):
+                self.placesStillToVisit.append(i)
+            i+=1
+
+    #Fonction permettant la selection d'un noeud en fonction des phéromones
+    def selectNode(self):
+        while True:
+            newPosition = randomPondereNoeud(self.currPosition)
+            if not(newPosition in self.visitedPlaces):
+                break
+
+        return newPosition
+
+    #Fonction permettant de déplacer la fourmi
+    def goToNextPosition(self):
+        newPosition = self.selectNode()
+        self.visitedPlaces.append(newPosition)
+        self.placesStillToVisit.remove(newPosition)
+        self.currPosition = newPosition
+
+
